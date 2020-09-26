e4e114969b41c6a3d233a9c890d88c12cab5e553
Jason Bacon <jwb@FreeBSD.org>
2020-09-25 20:48:43 -0500

    Add population variance

--- basic-stats-protos.h
+++ basic-stats-protos.h
@@ -3,4 +3,5 @@ int main(int argc, char *argv[]);
 void usage(char *argv[]);
 double median(void);
 int double_cmp(const double *d1, const double *d2);
+void population_variance(void);
 void average(void);
--- basic-stats.c
+++ basic-stats.c
@@ -22,24 +22,25 @@ int     main(int argc, char *argv[])
     /* options descriptor */
     // http://makemeanalyst.com/basic-statistics-for-data-analysis/
     static struct option longopts[] = {
-	{ "help",       required_argument,  NULL,           'h' },
-	{ "median",     required_argument,  NULL,           'm' },
-	{ "average",    required_argument,  NULL,           'a' },
-	{ "variance",   required_argument,  NULL,           'v' },
-	{ "stddev",     required_argument,  NULL,           's' },
-	{ "mode",       required_argument,  NULL,           'o' },
-	{ "range",      required_argument,  NULL,           'r' },
-	{ "iq-range",   required_argument,  NULL,           'i' },
-	{ "box-plot",   required_argument,  NULL,           'b' },
-	{ "z-scores",   required_argument,  NULL,           'z' },
-	{ 0,            0,                  0,              0 }
+	{ "help",               required_argument,  NULL,           'h' },
+	{ "median",             required_argument,  NULL,           'm' },
+	{ "average",            required_argument,  NULL,           'a' },
+	{ "population-variance",required_argument,  NULL,           'V' },
+	{ "sample-variance",    required_argument,  NULL,           'v' },
+	{ "stddev",             required_argument,  NULL,           's' },
+	{ "mode",               required_argument,  NULL,           'o' },
+	{ "range",              required_argument,  NULL,           'r' },
+	{ "iq-range",           required_argument,  NULL,           'i' },
+	{ "box-plot",           required_argument,  NULL,           'b' },
+	{ "z-scores",           required_argument,  NULL,           'z' },
+	{ 0,                    0,                  0,               0  }
     };
     int     ch;
     
     if ( argc < 2 )
 	usage(argv);
     
-    while ((ch = getopt_long(argc, argv, "m:a:", longopts, NULL)) != -1)
+    while ((ch = getopt_long(argc, argv, "m:a:V:", longopts, NULL)) != -1)
     {
 	switch(ch)
 	{
@@ -50,6 +51,9 @@ int     main(int argc, char *argv[])
 		break;
 	    case    'a':
 		average();
+		break;
+	    case    'V':
+		population_variance();
 		break;
 	    case    'v':
 	    case    's':
@@ -75,7 +79,8 @@ void    usage(char *argv[])
     fprintf(stderr, "\nAt least one of the following functions is required:\n\n");
     fprintf(stderr,"  --median, -m column\n");
     fprintf(stderr,"  --average, -a column\n");
-    fprintf(stderr,"  --variance, -v column\n");
+    fprintf(stderr,"  --population-variance, -V column\n");
+    fprintf(stderr,"  --sample-variance, -v column\n");
     fprintf(stderr,"  --stddev, -s column\n");
     fprintf(stderr,"  --mode, -o column\n");
     fprintf(stderr,"  --range, -r column\n");
@@ -124,6 +129,34 @@ int     double_cmp(const double *d1, const double *d2)
 	return -1;
     else
 	return 0;
+}
+
+
+void    population_variance(void)
+
+{
+    size_t  list_size,
+	    c;
+    double  num,
+	    total,
+	    mean;
+    FILE    *fp;
+    
+    fp = tmpfile();
+    for (list_size = 0, total = 0.0; scanf("%lf", &num) == 1; ++list_size)
+    {
+	total += num;
+	fprintf(fp, "%f\n", num);
+    }
+    mean = total / list_size;
+    
+    rewind(fp);
+    for (c = 0, total = 0.0; c < list_size; ++c)
+    {
+	fscanf(fp, "%lf", &num);
+	total += (num - mean) * (num - mean);
+    }
+    printf("%f\n", total / list_size);
 }
 
 
