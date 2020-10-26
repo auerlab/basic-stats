74ad6dbd5c5c0f8164a969f122b72d2f89b6811c
Jason Bacon <jwb@FreeBSD.org>
2020-10-26 15:31:21 -0500

    Document need for sample type constants to be 0 and 1

--- basic-stats.h
+++ basic-stats.h
@@ -5,6 +5,11 @@
 #define REALLOC(buff, count, type)  ((type *)realloc((buff), (count) * sizeof(type)))
 
 typedef double  variance_t;
+
+/* 
+ *  These are subtracted from the denominator when computing sample or
+ *  population variance, so they must be 0 and 1.
+ */
 #define POPULATION_VARIANCE 0.0
 #define SAMPLE_VARIANCE     1.0
 
